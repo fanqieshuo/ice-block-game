3f422b4388a9f0367b4a3aeb4b8d9c3c5cc66900
fanqieshuo <104495177+fanqieshuo@users.noreply.github.com>
2025-04-16 11:20:00 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,68 @@
+// 缓存名称和要缓存的文件
+const CACHE_NAME = 'ice-block-game-cache-v1';
+const urlsToCache = [
+  './',
+  './ice-block-game.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+// 安装Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('缓存创建成功');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 当浏览器请求资源时
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果在缓存中找到匹配的资源，则返回
+        if (response) {
+          return response;
+        }
+        // 不在缓存中则从网络获取
+        return fetch(event.request)
+          .then(response => {
+            // 检查是否是有效响应
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 克隆响应，因为响应流只能使用一次
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+});
+
+// 激活Service Worker时清理旧缓存
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
